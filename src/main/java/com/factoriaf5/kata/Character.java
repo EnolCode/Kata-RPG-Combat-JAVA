176f52344b79a33d6f6743c799b0bcd2646eed04
Enol-Igareta <enol1code@gmail.com>
2023-01-11 11:37:02 +0100

    trabajando

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,42 @@
 package com.factoriaf5.kata;
 
 public class Character {
+    private  Integer health = 1000;
+    private Integer level = 1;
+    private Boolean alive = true;
     
+    public Character() {
+        this.health = getHealth();
+        this.level = getLevel();
+        this.alive = getAlive();
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public void setHealth(Integer health) {
+        this.health = health;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Boolean getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Boolean alive) {
+        this.alive = alive;
+    }
+
+    public Integer damage (Integer dmg){
+        return  dmg - this.health;
+    }
+
 }
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -7,8 +7,29 @@ import org.junit.jupiter.api.Test;
 public class CharacterTest {
     
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    public void Health_is_1000() {
+        Character miCharacter = new Character();
+        assertEquals(1000,miCharacter.getHealth());
     }
 
+    @Test
+    public void level_is_1() {
+        Character miCharacter = new Character();
+        assertEquals(1,miCharacter.getLevel());
+    }
+
+    @Test
+    public void Character_Is_Alive() {
+        Character miCharacter = new Character();
+        assertEquals(true,miCharacter.getAlive());
+    }
+
+    // Nexts Tasks
+
+    @Test
+    public void damaged() {
+        Character miCharacter = new Character();
+        miCharacter.damage(500);
+        assertEquals(500,miCharacter.getHealth());
+    }
 }
